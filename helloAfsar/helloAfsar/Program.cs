f7966e0d613115a204827b47978489b7a2c9de90
Afsarud <engafsar5577@gmail.com>
2019-09-10 11:58:07 +0600

    firstProject

---
+++ helloAfsar/helloAfsar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloAfsar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fstName = "";
+            String sndName = "";
+            Console.WriteLine("Type your first name:");
+            fstName = Console.ReadLine();
+            Console.WriteLine("Hi: "+fstName);
+
+            Console.WriteLine("Type your Second name:");
+            sndName = Console.ReadLine();
+            Console.WriteLine("Hi: "+ sndName);
+            Console.WriteLine("Your Full Name: "+fstName  +sndName);
+            Console.ReadKey();
+        }
+    }
+}
